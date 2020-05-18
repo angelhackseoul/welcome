8444123b36f663db8d0bd3b7c03bec6c5cf0fd1f
LOGAN LEE <dlfjgkssk1@naver.com>
2020-05-18 14:46:29 +0900

    dummy commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+
+import styled from 'styled-components';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return <Wrapper>Winter is comming...</Wrapper>;
 }
+
+// delete me when the time comes.
+const Wrapper = styled.div`
+	background-color: black;
+	color: white;
+	height: 1000px;
+	text-align: center;
+	font-size: 100px;
+`;
 
 export default App;
