22d75f51f36e8fbc357e3c1d14872fcf4da99061
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-17 04:26:51 +0900

    storybook conf

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+	stories: ['../src/**/*.stories.t*'],
+	addons: [
+		'@storybook/preset-create-react-app',
+		'@storybook/addon-actions',
+		'@storybook/addon-knobs',
+		'@storybook/addon-links',
+	],
+};
