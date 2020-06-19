95a6a5a57be047eddd07814078c3b153d8a03a96
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-20 01:01:27 +0900

    add TopMenu

--- src/components/TopMenu/index.tsx
+++ src/components/TopMenu/index.tsx
@@ -1,85 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+// import { Zeplin } from 'components/Zeplin';
+import { Wrapper } from './Wrapper';
 import AngelHack_Logo from 'images/AngelHack_Logo.png';
+import Down from 'images/down.png';
+import sdefault from 'images/s-default.png';
 
 export const TopMenu = () => (
 	<Wrapper>
-		<span>
+		<span className='tab1'>
 			<img className='Logo' src={AngelHack_Logo} alt='AngelHack_Logo' />
 		</span>
-		<span href='#' className='t1 text'>
-			참여방법
+		<span className='tab2'>
+			<p className='Tab'>참여방법</p>
 		</span>
-		<span href='#' className='t2 text'>
-			도전과제
+
+		<span className='tab3'>
+			<p className='Tab'>도전과제</p>
 		</span>
-		<span className='t3 text'>상금</span>
-		<span href='#' className='t4 text'>
-			일정
+
+		<span className='tab4'>
+			<p className='Tab'>상금</p>
 		</span>
-		<span href='#' className='t5 text'>
-			심사기준
+
+		<span className='tab5'>
+			<p className='Tab'>일정</p>
 		</span>
-		<span className='t6'>한국어</span>
-		<span className='t7'>
-			<span className='text txt'>등록하기</span>
+		<span className='tab6'>
+			<p className='Tab'>심사기준</p>
 		</span>
+		<span className='Language-Menu'>
+			<p className='Tab'>한국어</p>
+			<img
+				alt='language-select'
+				src={Down}
+				// srcset='images/down@2x.png 2x,img/down@3x.png 3x'
+				className='Down'
+			/>
+		</span>
+		<img className='-Sdefault' src={sdefault} alt='등록하기' />
 	</Wrapper>
 );
-
-export const Wrapper = styled.div`
-	width: 1440px;
-	height: 80px;
-	background-color: #222222;
-	color: #ffffff;
-
-	.Logo {
-		margin: 20px 0px 21px 160px;
-		width: 200px;
-		height: 39px;
-		object-fit: contain;
-	}
-
-	.text {
-		height: 24px;
-		font-family: Helvetica;
-		font-size: 16px;
-		font-weight: bold;
-		font-stretch: normal;
-		font-style: normal;
-		line-height: 1.5;
-		letter-spacing: normal;
-	}
-
-	.t1 {
-		width: 59px;
-		margin: 28px 0px 28px 21.5px;
-	}
-	.t2 {
-		width: 59px;
-	}
-	.t3 {
-		width: 30px;
-	}
-
-	.t4 {
-		width: 30px;
-	}
-
-	.t5 {
-		width: 59px;
-	}
-
-	.t7 {
-		width: 123px;
-		height: 48px;
-		object-fit: contain;
-		border: solid 2px #e21c1c;
-
-		.txt {
-			width: 107px;
-		}
-	}
-`;
 
 export default TopMenu;
