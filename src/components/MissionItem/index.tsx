4f2f1468ca92d7555710267553bcb9d41df5cf3a
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-22 10:56:25 +0900

    add missions

--- src/components/MissionItem/index.tsx
+++ src/components/MissionItem/index.tsx
@@ -1,5 +1,43 @@
-import React from "react";
+import React from 'react';
+import down from 'images/down.svg';
 
-export const MissionItem = (props: any) => {
-    return <>Mission Item</>;
+import styled from 'styled-components';
+
+export const MissionItem = ({ name }: any) => {
+	return (
+		<Wrapper>
+			<div className='brick' />
+			<p className='text'>{name}</p>
+			<img src={down} className='down' alt='detailForEachGoal' />
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	height: 320px;
+	background-color: black;
+	margin-right: 48px;
+	margin-bottom: 48px;
+	text-align: center;
+
+	.brick {
+		padding-bottom: 240px;
+	}
+
+	.text {
+		height: 35px;
+		margin: 0px;
+		font-family: Helvetica;
+		font-size: 32px;
+		font-weight: bold;
+		font-stretch: normal;
+		font-style: normal;
+		line-height: 1.5;
+		letter-spacing: normal;
+		color: #ffffff;
+	}
+	.down {
+		height: 8px;
+		object-fit: contain;
+	}
+`;
--- src/sections/Missions/index.tsx
+++ src/sections/Missions/index.tsx
@@ -1,47 +1,47 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-import { MissionItem } from "components/MissionItem";
-import "./index.css";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { MissionItem } from 'components/MissionItem';
+import './index.css';
 
 export class Missions extends React.Component {
-    public render() {
-        return (
-            <section id="missions">
-                <Container>
-                    <h1 className="missions-heading">도전과제</h1>
-                    <p className="missions-message">
-                        <b>AngelHack Seoul 2020 Online</b> 해커톤은{" "}
-                        <span className="missions-emphasis">
-                            유엔이 제안하는 17가지의 지속 가능한 개발
-                            목표(Sustainable Development Goals)
-                        </span>
-                        을 바탕으로 설계되었으며, 이 해커톤에서는 아래 6개의
-                        선택 주제를 참가자들에게 제안합니다.
-                    </p>
-                    <Row>
-                        <Col>
-                            <MissionItem />
-                        </Col>
-                        <Col>
-                            <MissionItem />
-                        </Col>
-                        <Col>
-                            <MissionItem />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <MissionItem />
-                        </Col>
-                        <Col>
-                            <MissionItem />
-                        </Col>
-                        <Col>
-                            <MissionItem />
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        );
-    }
+	public render() {
+		return (
+			<section id='missions'>
+				<Container>
+					<h1 className='missions-heading'>도전과제</h1>
+					<p className='missions-message'>
+						<b>AngelHack Seoul 2020 Online</b> 해커톤은{' '}
+						<span className='missions-emphasis'>
+							유엔이 제안하는 17가지의 지속 가능한 개발
+							목표(Sustainable Development Goals)
+						</span>
+						을 바탕으로 설계되었으며, 이 해커톤에서는 아래 6개의
+						선택 주제를 참가자들에게 제안합니다.
+					</p>
+					<Row>
+						<Col>
+							<MissionItem name='교육' />
+						</Col>
+						<Col>
+							<MissionItem name='보건/의료' />
+						</Col>
+						<Col>
+							<MissionItem name='환경' />
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<MissionItem name='행정' />
+						</Col>
+						<Col>
+							<MissionItem name='불평등' />
+						</Col>
+						<Col>
+							<MissionItem name='경제' />
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		);
+	}
 }
