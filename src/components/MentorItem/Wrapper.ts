fc656d2b3bd9a94cc2c9f805eb52695f8d87318e
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-23 03:28:16 +0900

    add Mentors

---
+++ src/components/MentorItem/Wrapper.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	height: 256px;
+	border-radius: 4px;
+	background-color: #c4c4c4;
+`;
--- src/sections/Mentors/index.tsx
+++ src/sections/Mentors/index.tsx
@@ -1,42 +1,48 @@
-import React from "react";
-import { Container, Row, Col } from "reactstrap";
-import { MentorItem } from "components/MentorItem";
-import "./index.css";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { MentorItem } from 'components/MentorItem';
+import './index.css';
 
 export class Mentors extends React.Component {
-    public render() {
-        return (
-            <section id="mentors">
-                <Container>
-                    <h1 className="mentors-heading">멘토</h1>
-                    <p className="mentors-message">
-                        해커톤의 참가팀들을 위해 8명의 현업 멘토들이 멘토로
-                        참여하고 있습니다.
-                    </p>
-                    <Row>
-                        <Col>
-                            <MentorItem />
-                        </Col>
-                        <Col>
-                            <MentorItem />
-                        </Col>
-                        <Col>
-                            <MentorItem />
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col>
-                            <MentorItem />
-                        </Col>
-                        <Col>
-                            <MentorItem />
-                        </Col>
-                        <Col>
-                            <MentorItem />
-                        </Col>
-                    </Row>
-                </Container>
-            </section>
-        );
-    }
+	public render() {
+		return (
+			<section id='mentors'>
+				<Container>
+					<h1 className='mentors-heading'>멘토</h1>
+					<p className='mentors-message'>
+						해커톤의 참가팀들을 위해 8명의 현업 멘토들이 멘토로
+						참여하고 있습니다.
+					</p>
+					<Row>
+						<Col>
+							<MentorItem />
+						</Col>
+						<Col>
+							<MentorItem />
+						</Col>
+						<Col>
+							<MentorItem />
+						</Col>
+						<Col>
+							<MentorItem />
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<MentorItem />
+						</Col>
+						<Col>
+							<MentorItem />
+						</Col>
+						<Col>
+							<MentorItem />
+						</Col>
+						<Col>
+							<MentorItem />
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		);
+	}
 }
