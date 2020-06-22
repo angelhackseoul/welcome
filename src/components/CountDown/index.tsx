5374f4fcf71e3501d1989bef5e758c8b2ad988c9
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-22 11:41:15 +0900

    claening up directory

--- src/components/CountDown/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Wrapper } from './Wrapper';
-
-export const CountDown = () => (
-	<Wrapper>
-		<p className='guideText'>등록 마감까지 남은 시간</p>
-		<p className='countDown'>00:00:00</p>
-	</Wrapper>
-);
---
+++ src/components/ZeplinTest/index.tsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Wrapper } from './Wrapper';
+
+export const ZeplinTest = () => <Wrapper>ZeplinTest</Wrapper>;
