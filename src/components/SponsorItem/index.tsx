8c7535c243a56af29f8d1d08f7f285aa7f974bc4
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-24 03:55:35 +0900

    add sponsor

---
+++ src/components/SponsorItem/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Wrapper } from './Wrapper';
+
+export const SponsorItem = ({ img, level }: any) => {
+	// level 0 : 1160
+	// level 1 : 176
+	// level 2 : 128
+
+	let sponsor: string = '';
+
+	switch (level) {
+		case 0: {
+			sponsor = 'level0';
+			break;
+		}
+		case 1: {
+			sponsor = 'level1';
+			break;
+		}
+		case 2: {
+			sponsor = 'level2';
+			break;
+		}
+	}
+
+	//passing props to styled-comp has error
+	return (
+		<Wrapper>
+			<img className={sponsor} src={img} alt='sponsor-logo' />
+		</Wrapper>
+	);
+};
--- src/sections/Sponsors/index.tsx
+++ src/sections/Sponsors/index.tsx
@@ -1,15 +1,52 @@
-import React from "react";
-import { Container } from "reactstrap";
-import "./index.css";
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import { Wrapper } from './Wrapper';
+
+import { SponsorItem } from 'components/SponsorItem';
+
+//main
+import commonComputer from 'images/sponsor/commonComputer.png';
+
+//sub
+import fastCampus from 'images/sponsor/fastCampus.png';
+import tableManager from 'images/sponsor/tableManager.png';
+
+//others
+import beSide from 'images/sponsor/beSide.png';
+import dCmap from 'images/sponsor/dCmap.png';
 
 export class Sponsors extends React.Component {
-    public render() {
-        return (
-            <section id="sponsors">
-                <Container>
-                    <h1 className="sponsors-heading">후원</h1>
-                </Container>
-            </section>
-        );
-    }
+	public render() {
+		return (
+			<Wrapper id='sponsors'>
+				<Container>
+					<h1 className='sponsors-heading'>후원</h1>
+					<Row>
+						<Col md='12' lg='6'>
+							<SponsorItem level={0} img={commonComputer} />
+						</Col>
+						<Col md='12' lg='6'>
+							<SponsorItem level={0} img={commonComputer} />
+						</Col>
+					</Row>
+					<Row>
+						<Col md='6' lg={{ size: 5, offset: 1 }}>
+							<SponsorItem level={1} img={fastCampus} />
+						</Col>
+						<Col md='6' lg='5'>
+							<SponsorItem level={1} img={tableManager} />
+						</Col>
+					</Row>
+					<Row>
+						<Col md='4' lg={{ size: 4, offset: 2 }}>
+							<SponsorItem level={2} img={beSide} />
+						</Col>
+						<Col md='4' lg='4'>
+							<SponsorItem level={2} img={dCmap} />
+						</Col>
+					</Row>
+				</Container>
+			</Wrapper>
+		);
+	}
 }
