ab4762940b3ecc5366514b07bcade04d174c2cc5
LOGAN LEE <dlfjgkssk1@naver.com>
2020-06-24 05:30:02 +0900

    move to sponsor's webstie once click the logo

--- src/components/SponsorItem/index.tsx
+++ src/components/SponsorItem/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Wrapper } from './Wrapper';
 
-export const SponsorItem = ({ img, level }: any) => {
+export const SponsorItem = ({ img, level, url }: any) => {
 	// level 0 : 1160
 	// level 1 : 176
 	// level 2 : 128
@@ -25,7 +25,11 @@ export const SponsorItem = ({ img, level }: any) => {
 
 	//passing props to styled-comp has error
 	return (
-		<Wrapper>
+		<Wrapper
+			onClick={() => {
+				window.open(url); //This will open Google in a new window.
+			}}
+		>
 			<img className={sponsor} src={img} alt='sponsor-logo' />
 		</Wrapper>
 	);
--- src/sections/Sponsors/index.tsx
+++ src/sections/Sponsors/index.tsx
@@ -23,26 +23,50 @@ export class Sponsors extends React.Component {
 					<h1 className='sponsors-heading'>후원</h1>
 					<Row>
 						<Col md='12' lg='6'>
-							<SponsorItem level={0} img={commonComputer} />
+							<SponsorItem
+								level={0}
+								img={commonComputer}
+								url={`https://www.comcom.ai`}
+							/>
 						</Col>
 						<Col md='12' lg='6'>
-							<SponsorItem level={0} img={commonComputer} />
+							<SponsorItem
+								level={0}
+								img={commonComputer}
+								url={`https://www.comcom.ai`}
+							/>
 						</Col>
 					</Row>
 					<Row>
 						<Col md='6' lg={{ size: 5, offset: 1 }}>
-							<SponsorItem level={1} img={fastCampus} />
+							<SponsorItem
+								level={1}
+								img={fastCampus}
+								url={`https://www.fastcampus.co.kr/`}
+							/>
 						</Col>
 						<Col md='6' lg='5'>
-							<SponsorItem level={1} img={tableManager} />
+							<SponsorItem
+								level={1}
+								img={tableManager}
+								url={`http://www.tablemanager.io/`}
+							/>
 						</Col>
 					</Row>
 					<Row>
 						<Col md='4' lg={{ size: 4, offset: 2 }}>
-							<SponsorItem level={2} img={beSide} />
+							<SponsorItem
+								level={2}
+								img={beSide}
+								url={`http://www.bside.best/`}
+							/>
 						</Col>
 						<Col md='4' lg='4'>
-							<SponsorItem level={2} img={dCmap} />
+							<SponsorItem
+								level={2}
+								img={dCmap}
+								url={`https://dcamp.kr/dreambank`}
+							/>
 						</Col>
 					</Row>
 				</Container>
