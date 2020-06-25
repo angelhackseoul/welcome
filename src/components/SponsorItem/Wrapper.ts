201bf8f7b2149b66783e6fe5fc9f1b4db6551f4b
Seunghwan Hong <hj332921@gmail.com>
2020-06-25 19:36:59 +0900

    fix: 후원 부분 로고 비율 및 백그라운드 수정
    Signed-off-by: Seunghwan Hong <hj332921@gmail.com>

--- src/components/SponsorItem/Wrapper.ts
+++ src/components/SponsorItem/Wrapper.ts
@@ -1,28 +1,15 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Wrapper = styled.div`
-	background-color: #222222;
+    @media (max-width: 991px) {
+        margin-bottom: 16px;
+    }
 
-	@media (max-width: 991px) {
-		margin-bottom: 16px;
-	}
-
-	.level0 {
-		width: 100%;
-		height: 272px;
-	}
-	.level1 {
-		width: 100%;
-		height: 176px;
-	}
-	.level2 {
-		width: 100%;
-		height: 128px;
-	}
-	img {
-		display: block;
-		margin-left: auto;
-		margin-right: auto;
-		cursor: pointer;
-	}
+    img {
+        width: 75%;
+        display: block;
+        margin-left: auto;
+        margin-right: auto;
+        cursor: pointer;
+    }
 `;
