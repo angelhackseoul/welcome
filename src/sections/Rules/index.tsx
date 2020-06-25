f5cb09faaa2f131d50c74f3a7d7f2aa21baece9c
Seunghwan Hong <hj332921@gmail.com>
2020-06-25 18:47:11 +0900

    feat: 규칙 문구 수정
    Signed-off-by: Seunghwan Hong <hj332921@gmail.com>

--- src/sections/Rules/index.tsx
+++ src/sections/Rules/index.tsx
@@ -10,8 +10,11 @@ export class Rules extends React.Component {
                 <Container>
                     <h1 className="rules-heading">규칙</h1>
                     <p className="rules-desription">
-                        아래는 <b>AngelHack Seoul 2020 Online</b> 해커톤 (이하
-                        엔젤핵)의 7가지 규칙입니다.
+                        아래는 <b>AngelHack Seoul 2020 Online</b> 해커톤의 7가지
+                        규칙입니다. 또한 엔젤핵은 모든 이들에게 열려있는 즐거운
+                        행사를 만들기 위하여, 준수사항에 따라 운영하고 있습니다.
+                        모든 참가자, 멘토, 심사위원, 스폰서, 운영진들은 구분
+                        없이 준수 사항을 따라주시길 바랍니다.
                     </p>
                     <Row>
                         <Col md="12" lg="3">
@@ -85,8 +88,7 @@ const text: TextType[] = [
     },
     {
         ruleName: "6. 발표",
-        description: `최종 발표는 2분동안 진행
-        됩니다. 발표 후 심사위원들의 질의응답은 1분간 이뤄집니다.`,
+        description: `모든 참가 팀은 3분 이내의 피칭 녹화영상을 제출하여 결과물을 발표합니다.`,
     },
     {
         ruleName: "7. 좋은 동료",
