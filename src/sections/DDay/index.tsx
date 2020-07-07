e727e2cdb996273ced01a8bf689a6c9b9e008f54
Seunghwan Hong <hj332921@gmail.com>
2020-07-07 21:10:53 +0900

    feat: 행사 시작 시간 변경
    Signed-off-by: Seunghwan Hong <hj332921@gmail.com>

--- src/sections/DDay/index.tsx
+++ src/sections/DDay/index.tsx
@@ -1,41 +1,41 @@
-import React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import Countdown from 'react-countdown';
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import Countdown from "react-countdown";
 
-import './index.css';
+import "./index.css";
 
 export const DDay = () => {
-	const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
-		if (completed) {
-			return <span>참가 신청이 완료되었습니다!</span>;
-		} else {
-			return (
-				<span>
-					{days}d {hours}:{minutes}:{seconds}
-				</span>
-			);
-		}
-	};
-	return (
-		<section id='d-day'>
-			<Container>
-				<Row>
-					<Col xs='12' md='6'>
-						<span className='dday-heading'>
-							등록 마감까지 남은 시간
-						</span>
-					</Col>
-					<Col xs='12' md='6'>
-						<span className='dday-heading dday-heading-big'>
-							<Countdown
-								// zeroPadTime={2}
-								date={'2020/07/08 23:59:59'}
-								renderer={renderer}
-							/>
-						</span>
-					</Col>
-				</Row>
-			</Container>
-		</section>
-	);
+    const renderer = ({ days, hours, minutes, seconds, completed }: any) => {
+        if (completed) {
+            return <span>참가 신청이 완료되었습니다!</span>;
+        } else {
+            return (
+                <span>
+                    {days}d {hours}:{minutes}:{seconds}
+                </span>
+            );
+        }
+    };
+    return (
+        <section id="d-day">
+            <Container>
+                <Row>
+                    <Col xs="12" md="6">
+                        <span className="dday-heading">
+                            등록 마감까지 남은 시간
+                        </span>
+                    </Col>
+                    <Col xs="12" md="6">
+                        <span className="dday-heading dday-heading-big">
+                            <Countdown
+                                // zeroPadTime={2}
+                                date={"2020/07/10 23:59:59"}
+                                renderer={renderer}
+                            />
+                        </span>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
 };
