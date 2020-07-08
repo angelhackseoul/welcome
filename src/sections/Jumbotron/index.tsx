7501f61c5a4981528185994a229f6496db5180c1
Seunghwan Hong <hj332921@gmail.com>
2020-07-08 22:25:51 +0900

    feat: 행사 일정 추가
    Signed-off-by: Seunghwan Hong <hj332921@gmail.com>

--- src/sections/Jumbotron/index.tsx
+++ src/sections/Jumbotron/index.tsx
@@ -1,31 +1,36 @@
-import React from 'react';
-import { Container } from 'reactstrap';
-import './index.css';
+import React from "react";
+import { Container } from "reactstrap";
+import "./index.css";
 
 export const Jumbotron = (props: any) => {
-	return (
-		<section id='jumbotron'>
-			<Container>
-				<span className='jumbotron-heading'>
-					ANGELHACK <br />
-					SEOUL 2020 <br />
-					ONLINE
-				</span>
-				<p className='jumbotron-message'>
-					<b>AngelHack Seoul 2020 Online - “위기 속의 창의성”</b>은
-					COVID-19으로 인한 <br />
-					문제에 대처하기 위한 기술 솔루션을 만드는 것을 목표로 하는
-					온라인 해커톤입니다. <br />
-					<button
-						className='jumbotron-register'
-						onClick={() =>
-							window.open('https://forms.gle/gV58UM1VgUhCcjYv5')
-						}
-					>
-						지금 등록하기
-					</button>
-				</p>
-			</Container>
-		</section>
-	);
+    return (
+        <section id="jumbotron">
+            <Container>
+                <span className="jumbotron-heading">
+                    ANGELHACK <br />
+                    SEOUL 2020 <br />
+                    ONLINE
+                </span>
+                <p className="jumbotron-message">
+                    <b>AngelHack Seoul 2020 Online - “위기 속의 창의성”</b>은
+                    COVID-19으로 인한 <br />
+                    문제에 대처하기 위한 기술 솔루션을 만드는 것을 목표로 하는
+                    온라인 해커톤입니다. <br /> <br />
+                    <b>
+                        참가 신청 : ~ 7월 10일 (금) <br />
+                        행사 기간 : 7월 13일 (월) ~ 7월 19일 (일) <br />
+                        심사 기간 : 7월 20일 (월) ~ 7월 22일 (수) <br />
+                    </b>
+                    <button
+                        className="jumbotron-register"
+                        onClick={() =>
+                            window.open("https://forms.gle/gV58UM1VgUhCcjYv5")
+                        }
+                    >
+                        지금 등록하기
+                    </button>
+                </p>
+            </Container>
+        </section>
+    );
 };
